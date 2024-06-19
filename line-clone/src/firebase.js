6f83e6e704f153fd927d7551878557eb718b31cd
kon104 <toshiharu@MacBookAir2023.local>
2024-06-20 08:22:33 +0900

    updated file

--- line-clone/src/firebase.js
+++ line-clone/src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: "AIzaSyCB-R07Mc_-w5-nozS0jAxoxCoSnxSY71w",
-	authDomain: "line-clone-7890b.firebaseapp.com",
-	projectId: "line-clone-7890b",
-	storageBucket: "line-clone-7890b.appspot.com",
-	messagingSenderId: "228657850678",
-	appId: "1:228657850678:web:33bc3e3bc731b392112b97"
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: ""
 });
 
 const db = firebaseApp.firestore();
